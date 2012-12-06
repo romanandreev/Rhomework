168a5d8e21807e7dd73402d7b88c1e5d862cad94
romanandreev@gmail.com <romanandreev@gmail.com>
2012-12-07 00:26:11 +0400

    expr

--- hw2b.R
+++ hw2b.R
@@ -38,19 +38,20 @@ legend("topleft", c("Gamma distribution (2, 1)", "Exponential distribution (2)")
        fill = c("red", "blue"))
 
 windows()
-plot(ecdf(calcDist(f1, 100, 1000, Rabs)), col = "red", do.points=FALSE, lwd = 2, xlab="x", ylab="F(x)", 
-     main=expression(paste("Ecdf of R"[n], " for different distributions (N = 100, M = 1000, a = 0.3)")))
-plot(ecdf(calcDist(f2, 100, 1000, Rabs)), add = TRUE, col = "blue", do.points=FALSE, lwd = 2)
+plot(ecdf(calcDist(f1, 400, 1000, Rabs)), col = "red", do.points=FALSE, lwd = 2, xlab="x", ylab="F(x)", 
+     main=expression(paste("Ecdf of R"[n], " for different distributions (n = 400, M = 1000, a = 0.3)")))
+plot(ecdf(calcDist(f2, 400, 1000, Rabs)), add = TRUE, col = "blue", do.points=FALSE, lwd = 2)
 legend("topleft", c("Gamma distribution (2, 1)", "Exponential distribution (2)"), bty="n",
        fill = c("red", "blue"))
 
 windows()
-plot(ecdf(calcDist(f1, 100, 1000, Rplus)), col = "red", do.points=FALSE, lwd = 2, , xlab="x", ylab="F(x)", 
+plot(ecdf(calcDist(f1, 400, 1000, Rplus)), col = "red", do.points=FALSE, lwd = 2, , xlab="x", ylab="F(x)", 
      main=expression(paste("Ecdf for R"[n]^'+', " and its limit")))
 curve(pmax(2 * pnorm(x * sqrt(a / (1 - a))) - 1, 0) , col = "blue", lwd = 2, add = TRUE)
 
-legend("right", c("Gamma distribution (2, 1)", expression(textstyle(max) + bgroup("(", 2 %.% Phi + bgroup("(", x %.% sqrt(frac(a , 1 - a)) ,")") - 1 + ", 0" ,")")  )) , bty="n",
-       fill = c("red", "blue"))
+legend("right", c("Gamma distribution (2, 1)",  
+      expression(paste(textstyle(max), bgroup("(", 2 %.% paste(Phi, bgroup("(", x %.% paste(sqrt(frac(a , 1 - a)), " ") ,")") - 1 , ", 0") ,")")  ))),
+       fill = c("red", "blue"), bty="n")
 
 #expression(bgroup("(", 2 %.% Phi(x %.% sqrt(frac(a , (1 - a)))) - 1, 0) ,")")
 #plot(ecdf(calcDist(f1, 100, 100, Ra)), col = "red", do.points=FALSE, lwd = 2)
